b860e0cd9349e511117d96530b3f254d50c2027f
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-04-02 21:19:35 +0500

    installed styled-components & added some functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useState } from "react";
+
+const Billing = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 80px;
+`;
+
+const Tipping = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 80px;
+`;
 
 function App() {
+  const [total, setTotal] = useState(80);
+  const [rating, setRating] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Welcome</h1>
+      <Billing>
+        <p>How much was the bill?</p>
+        <input value={total} type="number" />
+      </Billing>
+      <Tipping>
+        <p>How did you like the service?</p>
+        <select>
+          <option value="low">Dissatisfied (0%)</option>
+          <option value="medium">Medium (5%)</option>
+          <option value="well">Well (10%)</option>
+        </select>
+      </Tipping>
+      <Tipping>
+        <p>How did your friend like the service?</p>
+        <select>
+           <option value="low">Dissatisfied (0%)</option>
+          <option value="medium">Medium (5%)</option>
+          <option value="well">Well (10%)</option>
+        </select>
+      </Tipping>
+      <p>Total: ${total} </p>
     </div>
   );
 }
